7da128436dd47e7b684c32b84eba11c7aca0e548
nyanpasu <atago@pomf.se>
2014-12-03 13:28:37 +0800

    Indents and more tweaks

--- xftwidth.c
+++ xftwidth.c
@@ -4,39 +4,39 @@
 #include <fontconfig/fontconfig.h>
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char** argv) {
-  if (argc < 3)
-  {
-    printf("xftwidth font string\n");
-    return 1;
-  }
+int main(int argc, char** argv)
+{
+        if (argc < 3)
+        {
+                printf("xftwidth font string\n");
+                return 1;
+        }
 
-  Display *dpy;
-  XftFont *fn;
-  XGlyphInfo ext;
-  FcChar8 str; 
+        Display *dpy;
+        XftFont *fn;
+        XGlyphInfo ext;
+        FcChar8 str; 
 
-  char *name = argv[1];
-  size_t len = strlen(argv[2]);
+        char *name = argv[1];
+        size_t len = strlen(argv[2]);
 
-  memcpy(&str, argv[2], len);
+        memcpy(&str, argv[2], len);
 
-  dpy = XOpenDisplay(NULL);
-  fn = XftFontOpenName(dpy, 0, name);
- 
-  if (fn == NULL)
-  {
-    puts("Font not found.");
-    return 1;
-  }
+        dpy = XOpenDisplay(NULL);
+        fn = XftFontOpenName(dpy, 0, name);
 
-  XftTextExtents8(dpy, fn, &str, (int)len, &ext);
-  printf("%d\n", ext.width);
- 
-  XCloseDisplay(dpy);
+        if (fn == NULL)
+        {
+                puts("Font not found.");
+                return 1;
+        }
 
-  return 0;
+        XftTextExtents8(dpy, fn, &str, (int)len, &ext);
+        printf("%d\n", ext.width);
+
+        XCloseDisplay(dpy);
+
+        return 0;
 }
